48490bdde537abf42ec4b1ccfbacc3b19dc2df36
Alex Ramirez <r.alexramirez@outlook.com>
2017-08-14 23:50:41 -0400

    Initial commit.

---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,9 @@
+(function() {
+  function HomeCtrl($scope, $firebaseArray) {
+    debugger;
+  }
+
+  angular
+    .module('blocitoff')
+    .controller('HomeCtrl', ["$scope", "$firebaseArray", HomeCtrl]);
+})();
