54cf53e2bf03861be58ff40530906d3e9d289a8d
Alex Ramirez <r.alexramirez@outlook.com>
2017-08-22 21:26:57 -0400

    hide tasks that are older than 7 days

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,32 @@
 (function() {
   function HomeCtrl($scope, $firebaseArray) {
-    debugger;
+    var ref = firebase.database().ref();
+    var tasksRef = ref.child("tasks");
+    var today = new Date();
+    var month = (today.getMonth()+1);
+    var day = today.getDate();
+    var year = today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes();
+    var age = (today.getDate()) - day
+
+    $scope.list = [];
+    $firebaseArray(tasksRef).$loaded().then(function(data){
+      for(var i = 0; i < data.length; i++) {
+        $scope.list.push(data[i]);
+      }
+    });
+
+    $scope.old = function(){
+      if (age == 1 ) {
+        return true
+      } else {
+        return false
+      }
+    }
   }
 
+
   angular
-    .module('blocitoff')
-    .controller('HomeCtrl', ["$scope", "$firebaseArray", HomeCtrl]);
+  .module('blocitoff')
+  .controller('HomeCtrl', ["$scope", "$firebaseArray", HomeCtrl]);
 })();
