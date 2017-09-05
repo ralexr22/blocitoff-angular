be7ebd012154b52ce49574490be2695b4c423ad5
Alex Ramirez <r.alexramirez@outlook.com>
2017-09-05 00:05:06 -0400

    checkpoints up to 7

--- app/scripts/controllers/HistCtrl.js
+++ app/scripts/controllers/HistCtrl.js
@@ -1,5 +1,20 @@
 (function() {
   function HistCtrl($scope, $firebaseArray) {
+    var ref = firebase.database().ref();
+    var tasksRef = ref.child("tasks");
+
+    $scope.list = [];
+    $firebaseArray(tasksRef).$loaded().then(function(data){
+      for(var i = 0; i < data.length; i++) {
+        var item = data[i];
+        var itemDate = new Date(item.date);
+        var sevenDaysAgo = new Date();
+        sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+        item.isOld = (itemDate <= sevenDaysAgo) || item.isCompleted 
+        $scope.list.push(item);
+        $scope.newTask ="";
+      }
+    });
   }
 
 angular
--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -2,19 +2,43 @@
   function HomeCtrl($scope, $firebaseArray) {
     var ref = firebase.database().ref();
     var tasksRef = ref.child("tasks");
-    var today = new Date();
-    var month = (today.getMonth()+1);
-    var day = today.getDate();
-    var year = today.getFullYear();
-    var time = today.getHours() + ":" + today.getMinutes();
-    var age = (today.getDate()) - day
 
     $scope.list = [];
+    $scope.newTask = {
+      name: "",
+      date: (new Date()).toLocaleDateString(),
+      isCompleted: false,
+      priority: 0
+    };
+
+    $scope.createNewTask = function() {
+      var tasks = $firebaseArray(tasksRef);
+      tasks.$add($scope.newTask).then(function(item){
+        console.log(item)
+        $scope.list.push(item);
+      });
+      // set $scope.newTask to a new object.
+      $scope.newTask = {
+        name: "",
+        date: (new Date()).toLocaleDateString(),
+        isCompleted: false,
+        priority: 0
+      };
+    };
+
+    $scope.toggleCompleted = function(item) {
+      item.isCompleted = true;
+      var taskRef = tasksRef.child(item.$id);
+      taskRef.update({ isCompleted: true })
+    }
+
     $firebaseArray(tasksRef).$loaded().then(function(data){
       for(var i = 0; i < data.length; i++) {
         var item = data[i];
-        var itemDate = new Date(item.date).getDate();
-        item.isOld = itemDate <= (day-7);
+        var itemDate = new Date(item.date);
+        var sevenDaysAgo = new Date();
+        sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+        item.isOld = (itemDate <= sevenDaysAgo) || item.isCompleted
         $scope.list.push(item);
       }
     });
